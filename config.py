31ddbb0f80fb19b3f49601021d828fc43c62f739
igorkunovski <igork110@mail.ru>
2022-11-20 19:04:00 +0200

    added methods for work with data base

--- config.py
+++ config.py
@@ -5,7 +5,63 @@ with sql.connect('main.db') as con:
 
 
 def show_all():
+    print("==============================================================")
+    print("| id | producer | mark | size | resolution | price | comment |")
+    print("==============================================================")
     cur.execute('''SELECT * FROM inventory''')
     for result in cur:
         print(result)
+    return
+
+
+def find(data):
+    print("==============================================================")
+    print("| id | producer | mark | size | resolution | price | comment |")
+    print("==============================================================")
+    cur.execute('''SELECT * FROM inventory''')
+    print("Found:")
+    for result in cur:
+        if (str(data)).upper() in str(result).upper():
+            print(result)
+            continue
+    return
+
+
+def insert():
+    print("Adding new device to inventory: ")
+    producer = input('Producer: ')
+    mark = input('Mark: ')
+    size = int(input('Size inch: '))
+    resolution = (input('Resolution: '))
+    price = float(input('Price, RUB: '))
+    comment = input('Comment: ')
+
+    cur.execute("INSERT INTO inventory (producer, mark, size, resolution, price, comment)" 
+                "VALUES" + f'("{producer}", "{mark}", {size}, "{resolution}", {price}, "{comment}");')
+    con.commit()
+    print('Successfully added to Data Base')
+    return
+
+
+def update(id: int, col_name: str, new_value):
+    cur.execute("UPDATE inventory "
+                "SET " + f'{col_name}={new_value} '
+                "WHERE inventory.id="f'{id};')
+    con.commit()
+    print('Data Successfully updated')
+    return
+
+
+def delete(id: int):
+    find(id)
+    print(" Are You sure You want to delete from inventory this item?")
+    choice = input("Y/N: ")
+    if choice.upper() == "Y":
+        cur.execute("DELETE FROM inventory "
+                    "WHERE inventory.id="f'{id};')
+        con.commit()
+        print('Data Successfully deleted')
+    else:
+        con.commit()
+        print('Data was not deleted')
     return
--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import config as db
 
 db.show_all()
+# db.insert()
+# db.update(7, 'price', '59000')
+# db.show_all()
+# db.delete(7)
+# db.show_all()
+# db.find('samsung')
